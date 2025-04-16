371af16cef48c041483ddb6f77c0817bc317b3eb
kcconsigo <kcconsigo@gmail.com>
2025-04-16 14:03:32 +0800

    Fixed: Refactored the code by implementing fixing on surround with try with of Scanner reader object for declare variable integers on java class

--- src/LeapYearProgram/LeapYearJavaProgram.java
+++ src/LeapYearProgram/LeapYearJavaProgram.java
@@ -1,5 +1,6 @@
 package LeapYearProgram;
 
+import java.time.LocalDate;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -28,49 +29,50 @@ public class LeapYearJavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner reader = new Scanner(System.in);
-		int year = 0;
-		int month = 0;
+		try (Scanner reader = new Scanner(System.in)) {
+			int year = 0;
+			int month = 0;
 
 
-		while(true) {
-			try {
-				System.out.println("\nType a year: ");
-				year = reader.nextInt();
-				System.out.println("\nType a month: ");
-				month = reader.nextInt();
+			while(true) {
+				try {
+					System.out.println("\nType a year: ");
+					year = reader.nextInt();
+					System.out.println("\nType a month: ");
+					month = reader.nextInt();
 
-				int days = daysinMonth(year,month);
+					int days = daysinMonth(year,month);
 
-				System.out.print(days);
+					System.out.print(days);
 
 
-				if (isLeapYear(year)) {
-					System.out.print("\nIt is Leap Year");
+					if (isLeapYear(year)) {
+						System.out.print("\nIt is Leap Year");
+					}
+					else {
+						System.out.print("\nIt is not Leap Year");
+					}
+
+
+				}catch (InputMismatchException | ArrayIndexOutOfBoundsException e) {
+					System.out.println("\nType a year: " +e.toString());
+					System.out.println("Please Enter Valid Year");
+					reader.next();
+					System.out.println("\nType a month: " +e.toString());
+					System.out.println("Please Enter Valid Months[1-12]");
+					reader.next();
+					continue;
+
+				}
+				if(year == 2020 && month == 2) {
+					System.out.println("\nYou entered: "+year);
+					System.out.println("You entered: "+month);
+					System.out.println("You met the last exact year and month of last data");
+					break;
 				}
 				else {
-					System.out.print("\nIt is not Leap Year");
+					System.out.println("\nIt's not done yet, you still haven't provided yet the exact year and month of last data");
 				}
-
-
-			}catch (InputMismatchException | ArrayIndexOutOfBoundsException e) {
-				System.out.println("\nType a year: " +e.toString());
-				System.out.println("Please Enter Valid Year");
-				reader.next();
-				System.out.println("\nType a month: " +e.toString());
-				System.out.println("Please Enter Valid Months[1-12]");
-				reader.next();
-				continue;
-
-			}
-			if(year == 2020 && month == 2) {
-				System.out.println("\nYou entered: "+year);
-				System.out.println("You entered: "+month);
-				System.out.println("You met the last exact year and month of last data");
-				break;
-			}
-			else {
-				System.out.println("\nIt's not done yet, you still haven't provided yet the exact year and month of last data");
 			}
 		}
 	}
